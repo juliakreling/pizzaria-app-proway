e005d23251b5b9146946f35b4da74edeb046c0df
Julia Gabriele Kreling <juliakreling14@gmail.com>
2025-08-27 22:51:22 -0300

    arrumado as portas

--- backend/app.py
+++ backend/app.py
@@ -108,5 +108,5 @@ def update_status(pedido_id):
     return jsonify(pedido)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port, debug=True)
